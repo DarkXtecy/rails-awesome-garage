a51d382570aef256719418b55772ed31fa307cd5
Mohammed-ZakiUllah Bhingraj <zakibhingraj@yahoo.com>
2025-01-17 18:49:00 +0000

    Finished partially still need to work on the styling of the favourite page

---
+++ app/controllers/pages_controller.rb
@@ -0,0 +1,5 @@
+class PagesController < ApplicationController
+  def home
+    # No logic needed here since this is a static page.
+  end
+end
--- db/seeds.rb
+++ db/seeds.rb
@@ -16,18 +16,57 @@ Car.destroy_all
 Owner.destroy_all
 
 # Seed Owners
-owner1 = Owner.create!(nickname: "Dipesh")
+owner1 = Owner.create!(nickname: "Clare")
 owner2 = Owner.create!(nickname: "Luke")
 owner3 = Owner.create!(nickname: "Sophia")
-owner4 = Owner.create!(nickname: "Ethan")
+owner4 = Owner.create!(nickname: "Matthew")
 owner5 = Owner.create!(nickname: "Emma")
 
 # Seed Cars
-car1 = Car.create!(brand: "Mercedes", model: "A-Class Hatchback", year: 2024, fuel: "Unleaded petrol", owner: owner2)
-car2 = Car.create!(brand: "Peugeot", model: "308", year: 2017, fuel: "Unleaded petrol", owner: owner1)
-car3 = Car.create!(brand: "BMW", model: "X5", year: 2022, fuel: "Diesel", owner: owner3)
-car4 = Car.create!(brand: "Audi", model: "A4", year: 2019, fuel: "Hybrid", owner: owner4)
-car5 = Car.create!(brand: "Tesla", model: "Model 3", year: 2021, fuel: "Electric", owner: owner5)
+car1 = Car.create!(
+  brand: "Mercedes",
+  model: "A-Class Hatchback",
+  year: 2024,
+  fuel: "Unleaded petrol",
+  owner: owner2,
+  image: "https://images.pexels.com/photos/205737/pexels-photo-205737.jpeg"
+)
+
+car2 = Car.create!(
+  brand: "Peugeot",
+  model: "308",
+  year: 2017,
+  fuel: "Unleaded petrol",
+  owner: owner1,
+  image: "https://images.pexels.com/photos/16636049/pexels-photo-16636049/free-photo-of-modern-convertible-car-on-road-in-park.jpeg"
+)
+
+car3 = Car.create!(
+  brand: "BMW",
+  model: "X5",
+  year: 2022,
+  fuel: "Diesel",
+  owner: owner3,
+  image: "https://images.pexels.com/photos/19909381/pexels-photo-19909381/free-photo-of-black-bmw-x5.jpeg"
+)
+
+car4 = Car.create!(
+  brand: "Audi",
+  model: "A4",
+  year: 2019,
+  fuel: "Hybrid",
+  owner: owner4,
+  image: "https://images.pexels.com/photos/19799319/pexels-photo-19799319/free-photo-of-black-audi-a4-convertible.jpeg"
+)
+
+car5 = Car.create!(
+  brand: "Tesla",
+  model: "Model 3",
+  year: 2021,
+  fuel: "Electric",
+  owner: owner5,
+  image: "https://images.pexels.com/photos/28123191/pexels-photo-28123191/free-photo-of-the-tesla-model-s-is-shown-in-this-image.jpeg"
+)
 
 # Seed Reviews
 Review.create!(comment: "Love this car!", rating: 5, car: car1)
